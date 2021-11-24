d31ce2253d75c0a40cf8619730e7d04ae1da445c
mivan999 <mivan999@mail.ru>
2021-11-24 20:33:59 +0700

    12. breakdown into components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img src=" https://lhtravel.ru/wp-content/uploads/2018/10/1-11.jpg" alt=""/>
+            <div>ava+descriptions</div>
+            <div>Mypost
+                <div>New post</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
