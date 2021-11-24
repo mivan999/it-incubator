8bfe1189433c2d0b543a83f7cfd67265824555cf
mivan999 <mivan999@mail.ru>
2021-11-24 14:48:26 +0700

    09. import export

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Technologies from "./Technologies";
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Technologies />
+      <Footer />
     </div>
   );
 }
