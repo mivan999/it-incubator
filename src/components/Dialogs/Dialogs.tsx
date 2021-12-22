096e2947e93f2077164a16a423f1409f3866c7e1
Ivan Mulyavin <mivan999@mail.ru>
2021-12-22 12:20:58 +0700

    25 lesson refactor use map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -60,24 +60,28 @@ const Dialogs = () => {
 
     ]
     let messageData:MessageDataType[]=[
-        {id:0,
-            message:"Hi"},
+        {
+            id:0,
+            message:"Hi"
+        },
         {id:1,
-            message:"Yo"}
+            message:"Yo"
+        }
     ]
+    let dialogsElement=dialogsData.map((d)=>
+        <DialogItem name={d.name} id={d.id}/>
+    )
+    let messageElement=messageData.map((m)=>
+        <Message message={m.message}/>
+    )
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
-
+                {dialogsElement}
             </div>
             <div className={s.messages}>
-                <Message message={messageData[0].message}/>
+                {messageElement}
+
                 <Message message={messageData[1].message}/>
             </div>
         </div>
